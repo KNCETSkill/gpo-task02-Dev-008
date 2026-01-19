d65a85417a4feef329d17985ae20aa152bb95b53
Dev-008 <devdharsan07@gmail.com>
2026-01-19 16:15:28 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class AttendanceStatusEvaluator{
-  public static void main(String[] args){
+  public static void main(String[]args){
     Scanner sc=new Scanner(System.in);
     System.out.println();
     int Attendance=sc.nextInt();
